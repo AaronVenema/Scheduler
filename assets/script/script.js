ad91e7bfbe9a5bf89e16515c52d29336119aaef4
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-09-24 17:30:50 -0500

    fixed jquery issue

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,45 +1,51 @@
+var data=[];
+
 function getDateTime() {
-    var currentDate = new Date();
-    var year    = currentDate.getFullYear();
-    var month   = currentDate.getMonth()+1; 
-    var day     = currentDate.getDate();
-    var hour    = currentDate.getHours();
-    var minute  = currentDate.getMinutes();
-    var second  = currentDate.getSeconds(); 
+     var currentDate = new Date();
+     var year    = currentDate.getFullYear();
+     var month   = currentDate.getMonth()+1; 
+     var day     = currentDate.getDate();
+     var hour    = currentDate.getHours();
+     var minute  = currentDate.getMinutes();
+     var second  = currentDate.getSeconds(); 
 
-    if(month.toString().length == 1) {
-         month = '0'+month;
-    }
-    if(day.toString().length == 1) {
-         day = '0'+day;
-    }   
-    if(hour.toString().length == 1) {
-         hour = '0'+hour;
-    }
-    if(minute.toString().length == 1) {
-         minute = '0'+minute;
-    }
-    if(second.toString().length == 1) {
-         second = '0'+second;
-    }   
-    var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
-     return dateTime;
-}
+     if(month.toString().length == 1) {
+          month = '0'+month;
+     }
+     if(day.toString().length == 1) {
+          day = '0'+day;
+     }   
+     if(hour.toString().length == 1) {
+          hour = '0'+hour;
+     }
+     if(minute.toString().length == 1) {
+          minute = '0'+minute;
+     }
+     if(second.toString().length == 1) {
+          second = '0'+second;
+     }   
+     var dateTime = month+'/'+day+'/'+year+' '+hour+':'+minute+':'+second;   
+      return dateTime;
+ }
 
-setInterval(function(){
-    dateTime = getDateTime();
-    document.getElementById("currentDay").innerHTML = dateTime;
-}, 1000);
+ setInterval(function(){
+     dateTime = getDateTime();
+     document.getElementById("currentDay").innerHTML = dateTime;
+ }, 1000);
 
 
-function saveUserInput () {
-    var userEvent = document.querySelector("#userEvent").value;
-    localStorage.setItem("userEvent", userEvent);
-    }
-function userData (){
-    var oldInput= localStorage.getItem("userEvent");
-    document.querySelector("#userEvent").value = oldInput
-}
-window.onload = function() {
-    userData();
-      };
+ function saveUserInput () {//saves
+     $('div[class="row time-block"]').each(function(){
+           let id = $(this).attr("id");
+           localStorage.setItem(id,$(this).children('textarea').val());
+      });
+ }
+ function userData (){//loads
+     $('div[class="row time-block"]').each(function(){
+           let id = $(this).attr("id");
+           $(this).children('textarea').val(localStorage.getItem(id));
+     });
+ }
+ window.onload = function() {
+     userData();
+ };
